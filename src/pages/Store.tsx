d6aea99f04f2c04ee4cd20045715252e37f70c04
SJRXD <rohr.olasz@gmail.com>
2022-11-19 19:31:45 -0800

    StoreItem component and props storeItems map

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,18 @@
+import storeItems from '../data/items.json';
+import { Row, Col } from 'react-bootstrap';
+import { StoreItem } from '../components/StoreItem';
+
 export function Store() {
-    return <h1>Store</h1>;
+    return <>
+    <h1>Store</h1>
+    <Row xs={1} md={2} lg={4} className="g-3">
+
+        {storeItems.map(item => (
+            <Col key={item.id}>
+            <StoreItem {...item}/>
+            </Col>
+        ))}
+
+    </Row>
+    </>
 }
